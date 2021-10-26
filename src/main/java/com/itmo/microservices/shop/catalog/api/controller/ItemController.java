e647449fa99613217f6cf02f1aef974775d67e9d
Destolaric <fireshowel@gmail.com>
2021-10-26 23:21:14 +0300

    [Controller]  Added /users endpoint

--- src/main/java/com/itmo/microservices/shop/catalog/api/controller/ItemController.java
+++ src/main/java/com/itmo/microservices/shop/catalog/api/controller/ItemController.java
@@ -2,29 +2,32 @@ package com.itmo.microservices.shop.catalog.api.controller;
 
 import com.itmo.microservices.shop.catalog.api.model.ItemDTO;
 import com.itmo.microservices.shop.catalog.impl.service.ItemService;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.repository.query.Param;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.UUID;
 
 @RestController
 @RequestMapping("/items")
+@AllArgsConstructor
 public class ItemController {
 
-    private final ItemService _itemService;
+  private ItemService itemService ;
 
-    @Autowired
-    ItemController(ItemService itemService) {
-        _itemService = itemService;
-    }
 
-    @GetMapping()
-    ItemDTO getAllItems() {
-        return null;
-    }
+  @GetMapping()
+  ResponseEntity<List<ItemDTO>> getAllItems(@RequestParam(value = "available") boolean available) {
+    return available ? new ResponseEntity<List<ItemDTO>>(itemService.getAvailableItems(),
+        HttpStatus.OK) : new ResponseEntity<List<ItemDTO>>(itemService.getItems(), HttpStatus.OK);
+  }
 
 
 }
