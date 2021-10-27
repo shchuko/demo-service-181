b25fc06e26631149e42c585ceab8ee4ebf608e47
Leonid Kairgeldin <ujkwf0at4vgb@mail.ru>
2021-10-27 14:47:57 +0300

    [test service] start develop integration tests

---
+++ src/test/java/com/itmo/microservices/shop/catalog/ItemServiceTest.java
@@ -0,0 +1,74 @@
+package com.itmo.microservices.shop.catalog;
+
+import com.itmo.microservices.shop.catalog.api.model.ItemDTO;
+import com.itmo.microservices.shop.catalog.impl.entity.Item;
+import com.itmo.microservices.shop.catalog.impl.mapper.ItemToItemDTOMapper;
+import com.itmo.microservices.shop.catalog.impl.repository.ItemRepository;
+import com.itmo.microservices.shop.catalog.impl.service.ItemService;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
+@SpringBootTest
+public class ItemServiceTest {
+    @MockBean
+    private ItemRepository itemRepository;
+
+    @Autowired
+    private ItemService itemService;
+
+    private final List<Item> mockedItems = Arrays.asList(
+            new Item(UUID.randomUUID(), "name1", 1, "desc1", 1),
+            new Item(UUID.randomUUID(), "name1", 2, "desc2", 0)
+    );
+
+    private List<ItemDTO> mockedItemsDTO = mockedItems.stream().map(ItemToItemDTOMapper.Companion::map).collect(Collectors.toList());
+
+
+    @Test
+    public void whenGetItems_thenReturnAllItemsIncludesWithZeroAmount(){
+        Mockito.when(itemRepository.findAll())
+                .thenReturn(mockedItems);
+
+        var test =  itemService.getItems();
+        var correct = mockedItemsDTO;
+        Assertions.assertEquals(correct, test);
+    }
+
+    @Test
+    public void whenGetAvailableItems_thenReturnAllItemsWithoutZeroAmount() {
+        Mockito.when(itemRepository.findAll())
+                .thenReturn(mockedItems);
+
+        var test =  itemService.getAvailableItems();
+        var correct = mockedItemsDTO.stream().filter(item -> item.getCount() > 0).collect(Collectors.toList());
+        Assertions.assertEquals(correct, test);
+    }
+
+    @Test
+    public void whenGetCountOfExistedItem_thenReturnCount() {
+        UUID testUUID = UUID.randomUUID();
+        Item testItem = new Item(testUUID, "name1", 1, "desc1", 1);
+        Mockito.when(itemRepository.getById(testUUID))
+                .thenReturn(testItem);
+
+
+        var test = itemService.getCountOfItem(testUUID);
+        var correct = testItem.getCount();
+        Assertions.assertEquals(correct, test);
+    }
+
+}
