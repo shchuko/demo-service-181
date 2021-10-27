8a124a186ec4bf419169e5925636ea2f2ca39629
Vladislav Yaroshchuk <yaroshchuk2000@gmail.com>
2021-10-28 00:15:49 +0300

    [demo] ISS-#4 drop demo project sources
    It was required to drop all the demo project sources
    because we use liquibase and now switching to
    `spring.jpa.hibernate.ddl-auto=validate` mode

--- src/main/kotlin/com/itmo/microservices/shop/demo/notifications/api/service/NotificationService.kt
+++
@@ -1,9 +0,0 @@
-package com.itmo.microservices.shop.demo.notifications.api.service
-
-import com.itmo.microservices.shop.demo.tasks.api.model.TaskModel
-import com.itmo.microservices.shop.demo.users.api.model.AppUserModel
-
-interface NotificationService {
-    fun processNewUser(user: AppUserModel)
-    fun processAssignedTask(task: TaskModel)
-}
