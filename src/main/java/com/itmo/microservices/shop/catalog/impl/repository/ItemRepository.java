d28f9db2f58d29e64372628212a222246405c1b9
Leonid Kairgeldin <ujkwf0at4vgb@mail.ru>
2021-10-27 13:03:52 +0300

    [fix] add Repository notation upper ItemRepository

--- src/main/java/com/itmo/microservices/shop/catalog/impl/repository/ItemRepository.java
+++ src/main/java/com/itmo/microservices/shop/catalog/impl/repository/ItemRepository.java
@@ -3,9 +3,11 @@ package com.itmo.microservices.shop.catalog.impl.repository;
 import com.itmo.microservices.shop.catalog.impl.entity.Item;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.UUID;
 
+@Repository
 public interface ItemRepository extends JpaRepository<Item, UUID> {
 }
