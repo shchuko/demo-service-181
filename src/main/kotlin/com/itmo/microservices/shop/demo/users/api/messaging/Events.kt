6a5eeb955463885668a8a7b0cbc3a2f9dbb76774
Vladislav Yaroshchuk <yaroshchuk2000@gmail.com>
2021-10-28 00:15:49 +0300

    [demo] ISS-#4 drop demo project sources
    It was required to drop all the demo project sources
    because we use liquibase and now switching to
    `spring.jpa.hibernate.ddl-auto=validate` mode

--- src/main/kotlin/com/itmo/microservices/shop/demo/users/api/messaging/Events.kt
+++
@@ -1,7 +0,0 @@
-package com.itmo.microservices.shop.demo.users.api.messaging
-
-import com.itmo.microservices.shop.demo.users.api.model.AppUserModel
-
-data class UserCreatedEvent(val user: AppUserModel)
-
-data class UserDeletedEvent(val username: String)
