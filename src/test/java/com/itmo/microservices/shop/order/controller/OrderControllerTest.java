e3a89422644bd3a3133ec7dbf0e5e7de59c0af08
Kozlov Alexey <61562916+OnlyOneUseAcc@users.noreply.github.com>
2021-11-02 20:37:58 +0300

    [order-service] #2 create `OrderController` controller
    * add `OrderController` controller and tests for it
    * add `IOrderService` interface for `OrderController`

---
+++ src/test/java/com/itmo/microservices/shop/order/controller/OrderControllerTest.java
@@ -0,0 +1,58 @@
+package com.itmo.microservices.shop.order.controller;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.itmo.microservices.shop.common.test.NoWebSecurityTestCase;
+import com.itmo.microservices.shop.order.HardcodedValues;
+import com.itmo.microservices.shop.order.api.model.OrderDTO;
+import com.itmo.microservices.shop.order.api.service.IOrderService;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ActiveProfiles("dev")
+@AutoConfigureMockMvc
+class OrderControllerTest extends NoWebSecurityTestCase {
+    @Autowired
+    private MockMvc mockMvc;
+    @MockBean
+    private IOrderService service;
+
+    private final static ObjectMapper mapper = new ObjectMapper();
+    private final HardcodedValues values = new HardcodedValues();
+
+    @Test
+    void createOrder() throws Exception {
+        OrderDTO orderDTO = new OrderDTO();
+        orderDTO.setUuid(values.orderUUID);
+        orderDTO.setTimeCreated(values.time);
+        orderDTO.setStatus(values.collectedStatus.getName());
+        orderDTO.setItemsMap(new HashMap<>());
+        orderDTO.setDeliveryDuration(values.slot);
+        orderDTO.setPaymentHistory(new ArrayList<>());
+
+        Mockito.when(service.createOrder()).thenReturn(orderDTO);
+        final String expectedResponseContent = mapper.writeValueAsString(orderDTO);
+
+        mockMvc.perform(post("/orders"))
+                .andDo(print())
+                .andExpect(status().isCreated())
+                .andExpect(content().contentType(MediaType.APPLICATION_JSON))
+                .andExpect(content().string(expectedResponseContent));
+
+        Mockito.verify(service).createOrder();
+        Mockito.verifyNoMoreInteractions(service);
+    }
+}
