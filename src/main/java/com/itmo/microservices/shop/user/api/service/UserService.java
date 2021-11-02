d6f12d7c60d01435d37dbed3b0c6acc076cf5e53
Danil Nasibullin aka Tulesha <tulesha_xochet_pit@mail.ru>
2021-11-02 18:02:29 +0300

    [user-service] #7 create `UserService` interface
    * Create `UserService` interface

---
+++ src/main/java/com/itmo/microservices/shop/user/api/service/UserService.java
@@ -0,0 +1,14 @@
+package com.itmo.microservices.shop.user.api.service;
+
+import com.itmo.microservices.shop.user.api.model.RegistrationRequest;
+import com.itmo.microservices.shop.user.api.model.UserModel;
+
+import java.util.UUID;
+
+public interface UserService {
+    void registerUser(RegistrationRequest request);
+
+    UserModel getUserByID(UUID uuid);
+
+    UserModel getUserByUsername(String username);
+}
