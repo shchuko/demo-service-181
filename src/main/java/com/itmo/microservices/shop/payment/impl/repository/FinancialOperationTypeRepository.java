efaa5b1f4bf95c7841bb51c4b984bee886c62fc4
paNoNi <novgorodcevnp@gmail.com>
2021-11-03 01:02:29 +0300

    [payment-service] #3 create repositories
    [payment-service] #3 clean code in repositories

---
+++ src/main/java/com/itmo/microservices/shop/payment/impl/repository/FinancialOperationTypeRepository.java
@@ -0,0 +1,9 @@
+package com.itmo.microservices.shop.payment.impl.repository;
+
+import com.itmo.microservices.shop.payment.impl.entity.FinancialOperationType;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FinancialOperationTypeRepository extends JpaRepository<FinancialOperationType, Integer> {
+
+    FinancialOperationType findFinancialOperationTypeById(Integer id);
+}
