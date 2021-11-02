21dadbc0d144455c9f1c63ded3721c0081bf6e60
paNoNi <novgorodcevnp@gmail.com>
2021-11-03 01:03:40 +0300

    [payment-service] #3 change payment entities
    [payment-service] #3 clean code in entities
    
    [payment-service] #3 remove entity name in PaymentStatus

--- src/main/java/com/itmo/microservices/shop/payment/impl/entity/FinancialOperationType.java
+++ src/main/java/com/itmo/microservices/shop/payment/impl/entity/FinancialOperationType.java
@@ -1,39 +1,41 @@
 package com.itmo.microservices.shop.payment.impl.entity;
 
-import java.util.Objects;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 import org.hibernate.Hibernate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.util.Objects;
 
 @Entity
 @Getter
 @Setter
 @ToString
+@AllArgsConstructor
 @RequiredArgsConstructor
 public class FinancialOperationType {
 
-  @Id
-  private Integer id;
-  private String name;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String name;
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) {
+            return false;
+        }
+        FinancialOperationType financialOperationType = (FinancialOperationType) o;
+        return Objects.equals(id, financialOperationType.getId());
     }
-    if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) {
-      return false;
-    }
-    FinancialOperationType financialOperationType = (FinancialOperationType) o;
-    return Objects.equals(id, financialOperationType.getId());
-  }
 
-  @Override
-  public int hashCode() {
-    return Objects.hashCode(id);
-  }
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
--- src/main/java/com/itmo/microservices/shop/payment/impl/entity/PaymentLogRecord.java
+++ src/main/java/com/itmo/microservices/shop/payment/impl/entity/PaymentLogRecord.java
@@ -1,68 +1,70 @@
 package com.itmo.microservices.shop.payment.impl.entity;
 
-import static org.hibernate.Hibernate.isInitialized;
-
-import java.util.Objects;
-import java.util.UUID;
-import javax.persistence.Entity;
-import javax.persistence.FetchType;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.Setter;
 import org.hibernate.Hibernate;
 
+import javax.persistence.*;
+import java.util.Objects;
+import java.util.UUID;
+
+import static org.hibernate.Hibernate.isInitialized;
+
 @Entity
 @Getter
 @Setter
+@AllArgsConstructor
 @RequiredArgsConstructor
 public class PaymentLogRecord {
 
-  @Id
-  private UUID id;
-  private Integer amount;
-  private Long timestamp;
-  private UUID orderId;
-  private UUID transactionId;
-  private UUID userId;
+    @Id
+    @GeneratedValue
+    private UUID id;
+    private Integer amount;
+    private Long timestamp;
+    private UUID orderId;
+    private UUID transactionId;
+    private UUID userId;
 
-  @ManyToOne(fetch = FetchType.LAZY)
-  private PaymentStatus paymentStatus;
 
-  @ManyToOne(fetch = FetchType.LAZY)
-  private FinancialOperationType financialOperationType;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private PaymentStatus paymentStatus;
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
+    @ManyToOne(fetch = FetchType.LAZY)
+    private FinancialOperationType financialOperationType;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) {
+            return false;
+        }
+        PaymentLogRecord paymentLogRecord = (PaymentLogRecord) o;
+        return Objects.equals(id, paymentLogRecord.getId());
     }
-    if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) {
-      return false;
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
     }
-    PaymentLogRecord paymentLogRecord = (PaymentLogRecord) o;
-    return Objects.equals(id, paymentLogRecord.getId());
-  }
 
-  @Override
-  public int hashCode() {
-    return Objects.hashCode(id);
-  }
-
-  @Override
-  public String toString() {
-    /* Using vanilla Java instead of lombok because of LAZY fetch type */
-    return "PaymentLogRecord(" +
-        "id=" + id +
-        ", amount=" + amount +
-        ", timestamp=" + timestamp +
-        ", orderId=" + orderId +
-        ", transactionId=" + transactionId +
-        ", userId=" + userId +
-        ", paymentStatus=" +
-        (isInitialized(paymentStatus) ? paymentStatus : "<NOT_FETCHED>") +
-        ", financialOperationType=" +
-        (isInitialized(financialOperationType) ? financialOperationType : "<NOT_FETCHED>") + ")";
-  }
+    @Override
+    public String toString() {
+        /* Using vanilla Java instead of lombok because of LAZY fetch type */
+        return "PaymentLogRecord(" +
+                "id=" + id +
+                ", amount=" + amount +
+                ", timestamp=" + timestamp +
+                ", orderId=" + orderId +
+                ", transactionId=" + transactionId +
+                ", userId=" + userId +
+                ", paymentStatus=" +
+                (isInitialized(paymentStatus) ? paymentStatus : "<NOT_FETCHED>") +
+                ", financialOperationType=" +
+                (isInitialized(financialOperationType) ? financialOperationType : "<NOT_FETCHED>") + ")";
+    }
 }
