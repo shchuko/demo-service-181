c91143ced8df36ec3f8248c218a5484d10a16812
Leonid Kairgeldin <man_in_jack@mail.ru>
2021-11-02 18:05:51 +0300

    [item service] #5 fix entity Item
    [item service] #5 clear code style & imports in entity Item

--- src/main/java/com/itmo/microservices/shop/catalog/impl/entity/Item.java
+++ src/main/java/com/itmo/microservices/shop/catalog/impl/entity/Item.java
@@ -1,44 +1,49 @@
 package com.itmo.microservices.shop.catalog.impl.entity;
 
 
+import lombok.*;
+import org.hibernate.Hibernate;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import java.util.Objects;
 import java.util.UUID;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
-import org.hibernate.Hibernate;
 
 @Entity
 @Getter
 @Setter
 @ToString
 @RequiredArgsConstructor
+@AllArgsConstructor
 public class Item {
 
-  @Id
-  private UUID id;
-  private String name;
-  private Integer price;
-  private String description;
-  private Integer amount;
+    @Id
+    @GeneratedValue
+    private UUID id;
+    private String name;
+    private Integer price;
+    private String description;
+    private Integer amount;
 
-  @Override
-  public boolean equals(Object o) {
-    if (this == o) {
-      return true;
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) {
+            return false;
+        }
+        Item item = (Item) o;
+        return Objects.equals(id, item.getId());
     }
-    if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) {
-      return false;
-    }
-    Item item = (Item) o;
-    return Objects.equals(id, item.getId());
-  }
 
-  @Override
-  public int hashCode() {
-    return Objects.hashCode(id);
-  }
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
 }
