32cfaecb0f1d1e44e6560b473c589bb6da541b37
Kozlov Alexey <61562916+OnlyOneUseAcc@users.noreply.github.com>
2021-11-02 20:22:41 +0300

    [order-service] #2 create `HardcodedValues`
    * class with base values for tests

---
+++ src/test/java/com/itmo/microservices/shop/order/HardcodedValues.java
@@ -0,0 +1,29 @@
+package com.itmo.microservices.shop.order;
+
+import com.itmo.microservices.shop.order.impl.entity.OrderStatus;
+
+import java.util.UUID;
+
+public class HardcodedValues {
+    public final UUID orderUUID = UUID.fromString("3fa85f64-5717-4562-b3fc-2c963f66afa6");
+    public final UUID userUUID = UUID.fromString("3fa85f64-5720-4562-b3fc-2c963f66afa6");
+    public final UUID itemUUID = UUID.fromString("3fa75f64-5720-4562-b3fc-2c963f66afa6");
+
+    public final Integer price = 1000;
+    public final Integer amount = 2;
+    public final Integer slot = 160256;
+    public final Long time = 456L;
+
+    public final OrderStatus collectedStatus;
+    public final OrderStatus bookedStatus;
+
+    {
+        collectedStatus = new OrderStatus();
+        collectedStatus.setId(1);
+        collectedStatus.setName("COLLECTED");
+
+        bookedStatus = new OrderStatus();
+        bookedStatus.setId(2);
+        bookedStatus.setName("BOOKED");
+    }
+}
