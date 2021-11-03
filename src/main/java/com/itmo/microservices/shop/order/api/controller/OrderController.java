2f3e559a86d2b3ec4149d7d0b51437dd124e8620
Kozlov Alexey <61562916+OnlyOneUseAcc@users.noreply.github.com>
2021-11-03 17:17:32 +0300

    [order-service] #2 add security options to controllers
    * add `SecurityRequirement` bearerAuth

--- src/main/java/com/itmo/microservices/shop/order/api/controller/OrderController.java
+++ src/main/java/com/itmo/microservices/shop/order/api/controller/OrderController.java
@@ -3,6 +3,8 @@ package com.itmo.microservices.shop.order.api.controller;
 import com.itmo.microservices.shop.order.api.model.BookingDTO;
 import com.itmo.microservices.shop.order.api.model.OrderDTO;
 import com.itmo.microservices.shop.order.api.service.IOrderService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -23,26 +25,31 @@ public class OrderController {
 
     @PostMapping()
     @ResponseStatus(HttpStatus.CREATED)
+    @Operation(security = {@SecurityRequirement(name = "bearerAuth")})
     ResponseEntity<OrderDTO> createOrder() {
         return new ResponseEntity<>(orderService.createOrder(), HttpStatus.CREATED);
     }
 
     @GetMapping("/{order_id}")
+    @Operation(security = {@SecurityRequirement(name = "bearerAuth")})
     ResponseEntity<OrderDTO> getOrder(@PathVariable UUID order_id){
         return new ResponseEntity<>(orderService.getOrder(order_id), HttpStatus.OK);
     }
 
     @PutMapping(value = "/{order_id}/items/{item_id}", params = {"amount"})
+    @Operation(security = {@SecurityRequirement(name = "bearerAuth")})
     void addItem(@PathVariable UUID order_id, @PathVariable UUID item_id, @RequestParam int amount){
         orderService.addItem(order_id, item_id, amount);
     }
 
     @PostMapping(value = "/{order_id}/delivery", params = {"slot"})
+    @Operation(security = {@SecurityRequirement(name = "bearerAuth")})
     ResponseEntity<BookingDTO> setTime(@PathVariable UUID order_id, @RequestParam int slot){
         return new ResponseEntity<>(orderService.setTime(order_id, slot), HttpStatus.OK);
     }
 
     @PostMapping(value = "/{order_id}/bookings")
+    @Operation(security = {@SecurityRequirement(name = "bearerAuth")})
     ResponseEntity<BookingDTO> finalizeOrder(@PathVariable UUID order_id){
         return new ResponseEntity<>(orderService.finalizeOrder(order_id), HttpStatus.OK);
     }
