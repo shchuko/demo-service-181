344c7bbd135eb74925b1f709e33b571e8d87be2c
Danil Nasibullin aka Tulesha <tulesha_xochet_pit@mail.ru>
2021-11-06 21:03:27 +0300

    [user-service] #28 add `email` field to `UserAuth`
    * Add email field to `UserAuth`
    * Refactor `JwtTokenManager` -> add `email` to token, implement `getEmailFromToken` function

--- src/main/java/com/itmo/microservices/shop/user/impl/userdetails/UserAuth.java
+++ src/main/java/com/itmo/microservices/shop/user/impl/userdetails/UserAuth.java
@@ -10,13 +10,19 @@ import java.util.UUID;
 
 public class UserAuth extends User {
     private final UUID uuid;
+    private final String email;
 
     public UUID getUuid() {
         return uuid;
     }
 
-    public UserAuth(UUID uuid, String username, String password, Collection<? extends GrantedAuthority> authorities) {
+    public String getEmail() {
+        return email;
+    }
+
+    public UserAuth(UUID uuid, String username, String password, String email, Collection<? extends GrantedAuthority> authorities) {
         super(username, password, authorities);
         this.uuid = uuid;
+        this.email = email;
     }
 }
