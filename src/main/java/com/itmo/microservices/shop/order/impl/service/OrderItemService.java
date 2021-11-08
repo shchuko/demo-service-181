509bb5794310b3fa77ac08550efc12065e4163c1
Kozlov Alexey <61562916+OnlyOneUseAcc@users.noreply.github.com>
2021-11-08 05:31:08 +0300

    [order-service] #31 pass order service logs to logger

--- src/main/java/com/itmo/microservices/shop/order/impl/service/OrderItemService.java
+++ src/main/java/com/itmo/microservices/shop/order/impl/service/OrderItemService.java
@@ -1,11 +1,14 @@
 package com.itmo.microservices.shop.order.impl.service;
 
+import com.itmo.microservices.commonlib.annotations.InjectEventLogger;
+import com.itmo.microservices.commonlib.logging.EventLogger;
 import com.itmo.microservices.shop.order.api.model.BookingDTO;
 import com.itmo.microservices.shop.order.api.model.OrderDTO;
 import com.itmo.microservices.shop.order.api.service.IOrderService;
 import com.itmo.microservices.shop.order.impl.entity.OrderItem;
 import com.itmo.microservices.shop.order.impl.entity.OrderStatus;
 import com.itmo.microservices.shop.order.impl.entity.OrderTable;
+import com.itmo.microservices.shop.order.impl.logging.OrderServiceNotableEvent;
 import com.itmo.microservices.shop.order.impl.mapper.OrderTableToOrderDTO;
 import com.itmo.microservices.shop.order.impl.repository.IOrderItemRepository;
 import com.itmo.microservices.shop.order.impl.repository.IOrderStatusRepository;
@@ -23,6 +26,8 @@ public class OrderItemService implements IOrderService {
     private final IOrderItemRepository itemRepository;
     private final IOrderStatusRepository statusRepository;
     private final IOrderTableRepository tableRepository;
+    @InjectEventLogger
+    private EventLogger eventLogger;
 
     public OrderItemService(IOrderItemRepository itemRepository,
                             IOrderStatusRepository statusRepository,
@@ -36,6 +41,9 @@ public class OrderItemService implements IOrderService {
     public OrderDTO createOrder() throws NoSuchElementException {
         Optional<OrderStatus> collectingStatusOptional = statusRepository.findOrderStatusByName("COLLECTING");
         if (collectingStatusOptional.isEmpty()) {
+            if (eventLogger != null) {
+                eventLogger.error(OrderServiceNotableEvent.E_NO_SUCH_STATUS, "COLLECTING");
+            }
             throw new NoSuchElementException(String.format("No status with name %s", "COLLECTING"));
         }
 
@@ -44,48 +52,47 @@ public class OrderItemService implements IOrderService {
         order.setStatus(collectingStatusOptional.get());
         order.setUserId(UUID.randomUUID()); // mock user UUID
         tableRepository.save(order);
+
+        if (eventLogger != null) {
+            eventLogger.info(OrderServiceNotableEvent.I_ORDER_CREATED, order.getId());
+        }
         return OrderTableToOrderDTO.toDTO(order);
     }
 
     @Override
     public OrderDTO getOrder(UUID orderUUID) throws NoSuchElementException {
-        Optional<OrderTable> order = tableRepository.findById(orderUUID);
-        if (order.isEmpty()) {
-            throw new NoSuchElementException(String.format("No order with uuid %s", orderUUID));
-        }
-        return OrderTableToOrderDTO.toDTO(order.get());
+        return OrderTableToOrderDTO.toDTO(getOrderByUUID(orderUUID));
     }
 
     @Override
     public void addItem(UUID orderUUID, UUID itemUUID, Integer amount) throws NoSuchElementException {
-        Optional<OrderTable> orderTableOptional = tableRepository.findById(orderUUID);
-        if (orderTableOptional.isEmpty()) {
-            throw new NoSuchElementException(String.format("No order with uuid %s", orderUUID));
-        }
-
+        OrderTable order = getOrderByUUID(orderUUID);
         OrderItem item = new OrderItem();
         item.setOrderId(orderUUID);
         item.setPrice(0); // mock price
-        item.setOrder(orderTableOptional.get());
+        item.setOrder(order);
         item.setAmount(amount);
         item.setItemId(itemUUID);
         itemRepository.save(item);
+
+        if (eventLogger != null) {
+            eventLogger.info(OrderServiceNotableEvent.I_ITEM_ADDED, itemUUID);
+        }
     }
 
     @Override
     public BookingDTO setTime(UUID orderUUID, Integer slot) throws NoSuchElementException {
-        Optional<OrderTable> orderTableOptional = tableRepository.findById(orderUUID);
-        if (orderTableOptional.isEmpty()) {
-            throw new NoSuchElementException(String.format("No order with uuid %s", orderUUID));
-        }
-
-        OrderTable order = orderTableOptional.get();
+        OrderTable order = getOrderByUUID(orderUUID);
         order.setDeliveryDuration(slot);
         tableRepository.save(order);
 
         BookingDTO bookingDTO = new BookingDTO();
         bookingDTO.setUuid(orderUUID);
         bookingDTO.setFailedItems(new HashSet<>());
+
+        if (eventLogger != null) {
+            eventLogger.info(OrderServiceNotableEvent.I_ORDER_SET_TIME, orderUUID);
+        }
         return bookingDTO; // mock BookingDTO
     }
 
@@ -93,21 +100,34 @@ public class OrderItemService implements IOrderService {
     public BookingDTO finalizeOrder(UUID orderUUID) throws NoSuchElementException {
         Optional<OrderStatus> finalizeStatusOptional = statusRepository.findOrderStatusByName("BOOKED");
         if (finalizeStatusOptional.isEmpty()) {
+            if (eventLogger != null) {
+                eventLogger.error(OrderServiceNotableEvent.E_NO_SUCH_STATUS, "BOOKED");
+            }
             throw new NoSuchElementException(String.format("No status with name %s", "BOOKED"));
         }
 
-        Optional<OrderTable> orderTableOptional = tableRepository.findById(orderUUID);
-        if (orderTableOptional.isEmpty()) {
-            throw new NoSuchElementException(String.format("No order with uuid %s", orderUUID));
-        }
-
-        OrderTable order = orderTableOptional.get();
+        OrderTable order = getOrderByUUID(orderUUID);
         order.setStatus(finalizeStatusOptional.get());
         tableRepository.save(order);
 
         BookingDTO bookingDTO = new BookingDTO();
         bookingDTO.setUuid(orderUUID);
         bookingDTO.setFailedItems(new HashSet<>());
+
+        if (eventLogger != null) {
+            eventLogger.info(OrderServiceNotableEvent.I_ORDER_BOOKED, orderUUID);
+        }
         return bookingDTO; // mock BookingDTO
+    }
+
+    private OrderTable getOrderByUUID(UUID orderUUID) {
+        Optional<OrderTable> orderTableOptional = tableRepository.findById(orderUUID);
+        if (orderTableOptional.isEmpty()) {
+            if (eventLogger != null) {
+                eventLogger.error(OrderServiceNotableEvent.E_NO_SUCH_ORDER, orderUUID);
+            }
+            throw new NoSuchElementException(String.format("No order with uuid %s", orderUUID));
+        }
+        return orderTableOptional.get();
     }
 }
