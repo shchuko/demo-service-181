8a24dcdb00dbf176309295a4091402e9ec3a3b36
Vladislav Yaroshchuk <yaroshchuk2000@gmail.com>
2021-11-09 19:16:24 +0300

    Revert "[user-service] #28 add `email` field to `UserAuth`"
    This reverts commit eee0ab740bd38243037fc2d68d80af56a5ef0ae5.

--- src/main/java/com/itmo/microservices/shop/user/impl/userdetails/UserAuth.java
+++ src/main/java/com/itmo/microservices/shop/user/impl/userdetails/UserAuth.java
@@ -10,19 +10,13 @@ import java.util.UUID;
 
 public class UserAuth extends User {
     private final UUID uuid;
-    private final String email;
 
     public UUID getUuid() {
         return uuid;
     }
 
-    public String getEmail() {
-        return email;
-    }
-
-    public UserAuth(UUID uuid, String username, String password, String email, Collection<? extends GrantedAuthority> authorities) {
+    public UserAuth(UUID uuid, String username, String password, Collection<? extends GrantedAuthority> authorities) {
         super(username, password, authorities);
         this.uuid = uuid;
-        this.email = email;
     }
 }
