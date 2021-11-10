554a040d05b495411d07811dbe9561a33c772844
Mikhail Nikolaev <cakenumber1@mail.ru>
2021-11-10 18:11:10 +0300

    [notification-service] add OrderFinalized process

--- src/main/kotlin/com/itmo/microservices/shop/notifications/api/service/NotificationService.kt
+++ src/main/kotlin/com/itmo/microservices/shop/notifications/api/service/NotificationService.kt
@@ -1,7 +1,9 @@
 package com.itmo.microservices.shop.notifications.api.service
 
+import com.itmo.microservices.shop.order.api.model.OrderDTO
 import com.itmo.microservices.shop.user.api.model.UserModel
 
 interface NotificationService {
     fun processNewUser(user: UserModel)
+    fun processOrderFinalized(order: OrderDTO)
 }
