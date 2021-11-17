82625eb510108ddb64d5be572e4a6f697de119d9
Xenia Savelicheva <savelicheva123@gmail.com>
2021-11-17 16:29:51 +0300

    [user-service] #29 create events for UserService and AuthService
    * Create notable events for UserService
    * Create notable events for AuthService

--- src/main/java/com/itmo/microservices/shop/user/impl/logging/UserServiceNotableEvents.java
+++ src/main/java/com/itmo/microservices/shop/user/impl/logging/UserServiceNotableEvents.java
@@ -4,23 +4,28 @@ import com.itmo.microservices.commonlib.logging.NotableEvent;
 import org.jetbrains.annotations.NotNull;
 
 public enum UserServiceNotableEvents implements NotableEvent {
-    I_USER_CREATED("User created: {}");
+    I_USER_CREATED("User created: {}"),
+    E_USER_ALREADY_CREATED("User already created: {}"),
+    I_USER_FOUND_BY_UUID("User found by uuid: {}"),
+    E_USER_NOT_FOUND_BY_UUID("User not found by uuid: {}"),
+    I_USER_FOUND_BY_NAME("User found by username: {}"),
+    E_USER_NOT_FOUND_BY_USERNAME("User not found by username: {}");
 
-    private final String user;
+    private final String template;
 
-    UserServiceNotableEvents(String user) {
-        this.user = user;
+    UserServiceNotableEvents(String template) {
+        this.template = template;
     }
 
     @NotNull
     @Override
     public String getName() {
-        return this.user;
+        return this.name();
     }
 
     @NotNull
     @Override
     public String getTemplate() {
-        return this.user;
+        return this.template;
     }
 }
