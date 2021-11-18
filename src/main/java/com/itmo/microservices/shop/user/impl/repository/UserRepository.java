812b490b526460fcfd7e632dfa544970bf29508c
Leonid Kairgeldin <man_in_jack@mail.ru>
2021-11-18 22:41:57 +0300

    [user-service] AdminController extend UserRepository

--- src/main/java/com/itmo/microservices/shop/user/impl/repository/UserRepository.java
+++ src/main/java/com/itmo/microservices/shop/user/impl/repository/UserRepository.java
@@ -4,9 +4,11 @@ import com.itmo.microservices.shop.user.impl.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
     User findByUsername(String username);
+    Optional<User> findById(UUID uuid);
 }
