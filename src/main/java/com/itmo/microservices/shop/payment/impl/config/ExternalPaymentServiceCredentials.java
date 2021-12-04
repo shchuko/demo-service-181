c7bb97426f9a323f630246b74be67e0f98f1cd3a
Vladislav Yaroshchuk <yaroshchuk2000@gmail.com>
2021-12-04 16:32:56 +0300

    [payment] Store external service credentials outside VSC

---
+++ src/main/java/com/itmo/microservices/shop/payment/impl/config/ExternalPaymentServiceCredentials.java
@@ -0,0 +1,36 @@
+package com.itmo.microservices.shop.payment.impl.config;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@ConfigurationProperties("external-payment-service")
+@Component
+public class ExternalPaymentServiceCredentials {
+    private String url;
+    private String syncSecret;
+    private String pollingSecret;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public String getSyncSecret() {
+        return syncSecret;
+    }
+
+    public void setSyncSecret(String syncSecret) {
+        this.syncSecret = syncSecret;
+    }
+
+    public String getPollingSecret() {
+        return pollingSecret;
+    }
+
+    public void setPollingSecret(String pollingSecret) {
+        this.pollingSecret = pollingSecret;
+    }
+}
