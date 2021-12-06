b3e735e36dd20e8f180993a064748f7cf13cfe9e
George <egor_kuzmin22@mail.ru>
2021-12-06 17:46:01 +0300

    [item] Fixed getters and setters in the Item class

--- src/main/java/com/itmo/microservices/shop/catalog/api/model/ItemDTO.java
+++ src/main/java/com/itmo/microservices/shop/catalog/api/model/ItemDTO.java
@@ -1,15 +1,11 @@
 package com.itmo.microservices.shop.catalog.api.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import org.hibernate.Hibernate;
 
+import java.util.Objects;
 import java.util.UUID;
 
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 public class ItemDTO {
     private UUID id;
 
@@ -20,4 +16,67 @@ public class ItemDTO {
     private int price;
     private int amount;
 
+    public ItemDTO(UUID id, String name, String description, int price, int amount) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+        this.amount = amount;
+    }
+
+    public ItemDTO() {
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        ItemDTO itemDTO = (ItemDTO) o;
+        return Objects.equals(id, itemDTO.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
