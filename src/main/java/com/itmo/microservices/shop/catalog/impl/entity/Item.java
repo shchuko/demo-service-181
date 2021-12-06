0b253f82dcd08674f3cd744c43bab0bbf8d1a103
George <egor_kuzmin22@mail.ru>
2021-12-06 17:46:01 +0300

    [item] Fixed getters and setters in the Item class

--- src/main/java/com/itmo/microservices/shop/catalog/impl/entity/Item.java
+++ src/main/java/com/itmo/microservices/shop/catalog/impl/entity/Item.java
@@ -1,7 +1,5 @@
 package com.itmo.microservices.shop.catalog.impl.entity;
 
-
-import lombok.*;
 import org.hibernate.Hibernate;
 
 import javax.persistence.Entity;
@@ -11,13 +9,7 @@ import java.util.Objects;
 import java.util.UUID;
 
 @Entity
-@Getter
-@Setter
-@ToString
-@RequiredArgsConstructor
-@AllArgsConstructor
 public class Item {
-
     @Id
     @GeneratedValue
     private UUID id;
@@ -25,6 +17,18 @@ public class Item {
     private Integer price;
     private String description;
     private Integer amount;
+
+    public Item(UUID id, String name, Integer price, String description, Integer amount) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.amount = amount;
+    }
+
+    public Item() {
+
+    }
 
     @Override
     public boolean equals(Object o) {
@@ -43,7 +47,54 @@ public class Item {
         return Objects.hashCode(id);
     }
 
+    public Integer getPrice() {
+        return price;
+    }
+
+    public void setPrice(Integer price) {
+        this.price = price;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
     public Integer getAmount() {
         return amount;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", price=" + price +
+                ", description='" + description + '\'' +
+                ", amount=" + amount +
+                '}';
     }
 }
