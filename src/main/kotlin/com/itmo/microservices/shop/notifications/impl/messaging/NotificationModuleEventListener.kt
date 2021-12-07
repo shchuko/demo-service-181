51602afdab7c2bff3678f1d400d1a9157137270e
Cakenumber1 <52410533+Cakenumber1@users.noreply.github.com>
2021-12-07 09:29:45 +0300

    [notification] Add notification handler for finalized order (#44)

--- src/main/kotlin/com/itmo/microservices/shop/notifications/impl/messaging/NotificationModuleEventListener.kt
+++ src/main/kotlin/com/itmo/microservices/shop/notifications/impl/messaging/NotificationModuleEventListener.kt
@@ -6,6 +6,7 @@ import com.itmo.microservices.commonlib.annotations.InjectEventLogger
 import com.itmo.microservices.commonlib.logging.EventLogger
 import com.itmo.microservices.shop.common.logging.CommonNotableEvents
 import com.itmo.microservices.shop.notifications.api.service.NotificationService
+import com.itmo.microservices.shop.order.messaging.OrderCreatedEvent
 import com.itmo.microservices.shop.user.api.messaging.UserCreatedEvent
 import org.springframework.stereotype.Component
 import java.util.concurrent.ExecutorService
@@ -24,6 +25,17 @@ class NotificationModuleEventListener(private val notificationService: Notificat
     @AllowConcurrentEvents
     fun accept(event: UserCreatedEvent) = executor.execute {
         notificationService.processNewUser(event.userModel)
+        eventLogger.info(
+            CommonNotableEvents.I_LISTENER_RECEIVED_MESSAGE,
+            event
+        )
+
+    }
+
+    @Subscribe
+    @AllowConcurrentEvents
+    fun accept(event: OrderCreatedEvent) = executor.execute {
+        notificationService.processOrderFinalized(event.orderDTO)
         eventLogger.info(
             CommonNotableEvents.I_LISTENER_RECEIVED_MESSAGE,
             event
--- src/main/kotlin/com/itmo/microservices/shop/notifications/impl/service/StubNotificationService.kt
+++ src/main/kotlin/com/itmo/microservices/shop/notifications/impl/service/StubNotificationService.kt
@@ -1,6 +1,7 @@
 package com.itmo.microservices.shop.notifications.impl.service
 
 import com.itmo.microservices.shop.notifications.api.service.NotificationService
+import com.itmo.microservices.shop.order.api.model.OrderDTO
 import com.itmo.microservices.shop.user.api.model.UserModel
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -16,6 +17,10 @@ class StubNotificationService() : NotificationService {
     override fun processNewUser(user: UserModel) {
 
         log.info("User ${user.username} was created & should be notified")
+    }
+
+    override fun processOrderFinalized(order: OrderDTO) {
+        log.info("Order ${order.uuid} was created & user should be notified")
     }
 
 }
