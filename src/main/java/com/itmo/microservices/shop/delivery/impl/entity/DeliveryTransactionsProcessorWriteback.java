f3858ca16f426459770a022a9adf91f15cf3423f
Xenia Savelicheva <savelicheva123@gmail.com>
2021-12-08 20:20:08 +0300

    [delivery-service] #64 Add transaction polling writeback table schema

---
+++ src/main/java/com/itmo/microservices/shop/delivery/impl/entity/DeliveryTransactionsProcessorWriteback.java
@@ -0,0 +1,34 @@
+package com.itmo.microservices.shop.delivery.impl.entity;
+
+import lombok.*;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.Objects;
+import java.util.UUID;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@RequiredArgsConstructor
+@ToString
+public class DeliveryTransactionsProcessorWriteback {
+    @Id
+    private UUID id;
+    private UUID orderId;
+    private UUID userId;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        DeliveryTransactionsProcessorWriteback that = (DeliveryTransactionsProcessorWriteback) o;
+        return Objects.equals(id, that.getId());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
