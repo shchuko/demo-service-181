cbca52d09fdcd0d9206004b591653a19cc775e4a
Leonid Kairgeldin <man_in_jack@mail.ru>
2021-12-11 14:12:49 +0300

    [item-service] gh-70 add methods to use them when OrderService injection
    * implement booking methods for itemService

--- src/main/java/com/itmo/microservices/shop/catalog/api/service/ItemService.java
+++ src/main/java/com/itmo/microservices/shop/catalog/api/service/ItemService.java
@@ -3,9 +3,22 @@ package com.itmo.microservices.shop.catalog.api.service;
 import com.itmo.microservices.shop.catalog.api.model.ItemDTO;
 
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 public interface ItemService {
+
+    //region injectionOrderService
+
+    ItemDTO getByUuid(UUID uuid);
+
+    List<UUID> bookItems(Map<UUID, Integer> bookMap);
+
+    List<UUID> deleteBooking(Map<UUID, Integer> bookMap);
+    //endregion
+
+    //region controllerUsedMethods
+
     List<ItemDTO> getItems();
 
     List<ItemDTO> getAvailableItems();
@@ -19,6 +32,7 @@ public interface ItemService {
     void createItem(ItemDTO item);
 
     void updateItem(UUID uuid, ItemDTO itemDTO);
+    //endregion
 
-    boolean increaseItemAmount(int diff, UUID itemUuid);
+
 }
