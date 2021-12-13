fd2054d06d8bdc1b388fdf2dab739fcd014af43a
Danil Nasibullin aka Tulesha <tulesha_xochet_pit@mail.ru>
2021-12-13 19:19:03 +0300

    [order-service] #72 delete lombock methods

--- src/main/java/com/itmo/microservices/shop/order/api/model/OrderDTO.java
+++ src/main/java/com/itmo/microservices/shop/order/api/model/OrderDTO.java
@@ -1,13 +1,13 @@
 package com.itmo.microservices.shop.order.api.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import lombok.Data;
+import org.hibernate.Hibernate;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.UUID;
 
-@Data
 public class OrderDTO {
     @JsonProperty("id")
     private UUID uuid;
@@ -26,4 +26,65 @@ public class OrderDTO {
 
     @JsonProperty("paymentHistory")
     private List<PaymentLogRecord> paymentHistory;
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
+
+    public long getTimeCreated() {
+        return timeCreated;
+    }
+
+    public void setTimeCreated(long timeCreated) {
+        this.timeCreated = timeCreated;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Map<UUID, Integer> getItemsMap() {
+        return itemsMap;
+    }
+
+    public void setItemsMap(Map<UUID, Integer> itemsMap) {
+        this.itemsMap = itemsMap;
+    }
+
+    public long getDeliveryDuration() {
+        return deliveryDuration;
+    }
+
+    public void setDeliveryDuration(long deliveryDuration) {
+        this.deliveryDuration = deliveryDuration;
+    }
+
+    public List<PaymentLogRecord> getPaymentHistory() {
+        return paymentHistory;
+    }
+
+    public void setPaymentHistory(List<PaymentLogRecord> paymentHistory) {
+        this.paymentHistory = paymentHistory;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
+        OrderDTO orderDTO = (OrderDTO) o;
+        return Objects.equals(uuid, orderDTO.uuid);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(uuid);
+    }
 }
