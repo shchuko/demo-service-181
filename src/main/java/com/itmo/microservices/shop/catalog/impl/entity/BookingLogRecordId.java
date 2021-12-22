ae79c3e8cf4474b2e9975986ecb69713ab683285
Vladislav Yaroshchuk <yaroshchuk2000@gmail.com>
2021-12-22 11:07:51 +0300

    [item-service] Create schema for booking service

---
+++ src/main/java/com/itmo/microservices/shop/catalog/impl/entity/BookingLogRecordId.java
@@ -0,0 +1,26 @@
+package com.itmo.microservices.shop.catalog.impl.entity;
+
+import javax.persistence.Embeddable;
+import java.io.Serializable;
+import java.util.Objects;
+import java.util.UUID;
+
+@Embeddable
+public class BookingLogRecordId implements Serializable {
+    private UUID bookingId;
+    private UUID itemId;
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        BookingLogRecordId that = (BookingLogRecordId) o;
+        return Objects.equals(bookingId, that.bookingId) && Objects.equals(itemId, that.itemId);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(bookingId, itemId);
+    }
+}
