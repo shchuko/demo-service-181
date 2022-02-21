3a90c11737406bdd086c2630acf107cfa74f83cf
Vladislav Yaroshchuk <Vladislav.Yaroshchuk@jetbrains.com>
2022-02-21 07:38:40 +0300

    [order-service] add more logging

--- src/main/java/com/itmo/microservices/shop/order/impl/logging/OrderServiceNotableEvent.java
+++ src/main/java/com/itmo/microservices/shop/order/impl/logging/OrderServiceNotableEvent.java
@@ -6,19 +6,27 @@ import org.jetbrains.annotations.NotNull;
 public enum OrderServiceNotableEvent implements NotableEvent {
     // info events
     I_ORDER_CREATED("Create order {}"),
-    I_ORDER_BOOKED("Booked order {}"),
+    I_ORDER_FINALIZED("Booked order {}"),
+    I_ORDER_UNBOOKED("Booking cancelled for order {}"),
     I_ORDER_START_DELIVERY("Order {} delivery start"),
-    I_ORDER_END_DELIVERY("Order {} delivery end"),
+    I_ORDER_SUCCESSFUL_DELIVERY("Order {} delivery success"),
     I_ORDER_FAILED_DELIVERY("Order {} delivery failed"),
-    I_ORDER_SET_TIME("Order {} delivery time set"),
+    I_ORDER_SUBMIT_PAYMENT("Order {} payment submitted"),
+    I_ORDER_SUCCESSFUL_PAYMENT("Order {} payment success"),
+    I_ORDER_FAILED_PAYMENT("Order {} payment failed"),
+    I_ORDER_CANCELLED_PAYMENT("Order {} payment cancelled"),
+    I_ORDER_SET_TIME_SLOT("Order {} delivery time slot set"),
     I_ITEM_ADDED("Item with UUID {} added to order"),
+    I_REFUND_STARTED("Refund started for order {}"),
+    I_REFUND_DONE("Refund done for order {}"),
 
     // error events
     E_NO_SUCH_ORDER("No such order with UUID {}"),
-    E_NO_SUCH_STATUS("No such status with name {}"),
-    E_ORDER_ALREADY_BOOKED("Order with UUID {} was already booked"), // future feature
-    E_ITEM_NOT_FOUND("Item service send error {}"), // future feature
-    E_ACCESS_TO_FOREIGN_ORDER("User UUID {} is not equal to user UUID in order"); // future feature
+    E_ORDER_SUBMIT_PAYMENT("Order payment submit error: already exists for order {}"),
+    E_CONFLICT("Conflict while order processing {}"),
+    E_ILLEGAL_STATE("Illegal state {}"),
+    E_ITEM_NOT_FOUND("Item not found {}"),
+    E_NOT_ALLOWED("Attempt to access order {} rejected");
 
     private final String template;
 
--- src/main/java/com/itmo/microservices/shop/order/impl/service/OrderItemService.java
+++ src/main/java/com/itmo/microservices/shop/order/impl/service/OrderItemService.java
@@ -4,6 +4,7 @@ import com.google.common.eventbus.EventBus;
 import com.google.common.eventbus.Subscribe;
 import com.itmo.microservices.commonlib.annotations.InjectEventLogger;
 import com.itmo.microservices.commonlib.logging.EventLogger;
+import com.itmo.microservices.commonlib.logging.NotableEvent;
 import com.itmo.microservices.shop.catalog.api.exceptions.ItemNotFoundException;
 import com.itmo.microservices.shop.catalog.api.model.BookingDescriptionDto;
 import com.itmo.microservices.shop.catalog.api.model.ItemDTO;
@@ -33,6 +34,7 @@ import com.itmo.microservices.shop.payment.impl.exceptions.PaymentAlreadyExistsE
 import com.itmo.microservices.shop.payment.impl.exceptions.PaymentInUninterruptibleProcessing;
 import com.itmo.microservices.shop.payment.impl.exceptions.PaymentInfoNotFoundException;
 import com.itmo.microservices.shop.user.api.service.UserService;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Service;
 
 import java.time.Instant;
@@ -80,12 +82,9 @@ public class OrderItemService implements IOrderService {
         order.setUserId(userId);
         orderRepository.save(order);
 
-        if (eventLogger != null) {
-            eventLogger.info(OrderServiceNotableEvent.I_ORDER_CREATED, order.getId());
-        }
-
         OrderDTO orderDTO = OrderTableToOrderDTO.toDTO(order);
         eventBus.post(new OrderCreatedEvent(orderDTO));
+        logInfo(OrderServiceNotableEvent.I_ORDER_CREATED, order.getId());
         return orderDTO;
     }
 
@@ -113,15 +112,18 @@ public class OrderItemService implements IOrderService {
                     throw new RuntimeException("PaymentInfoNotFoundException: Should not be reached", e);
                 } catch (PaymentInUninterruptibleProcessing e) {
                     /* Cannot cancel payment - already paid, transaction in progress */
+                    logError(OrderServiceNotableEvent.E_CONFLICT, "'AddItem' cannot be performed because of payment processing, order=" + orderId);
                     throw new BadOperationForCurrentOrderStatus("'AddItem' cannot be performed because of payment processing", orderId);
                 }
 
                 order.setStatus(statusRepository.findOrderStatusByName(IOrderStatusRepository.StatusNames.COLLECTING.name()));
                 itemService.cancelBooking(order.getLastBookingId());
                 orderRepository.save(order);
+                logInfo(OrderServiceNotableEvent.I_ORDER_UNBOOKED, orderId);
                 break;
 
             default:
+                logError(OrderServiceNotableEvent.E_CONFLICT, "'AddItem' allowed only for 'BOOKED' or 'COLLECTING' order=" + orderId + " status=" + status);
                 throw new BadOperationForCurrentOrderStatus("'AddItem' allowed only for 'BOOKED' or 'COLLECTING' order", orderId, status);
         }
 
@@ -129,9 +131,7 @@ public class OrderItemService implements IOrderService {
         try {
             item = itemService.describeItem(itemId);
         } catch (ItemNotFoundException exception) {
-            if (eventLogger != null) {
-                eventLogger.error(OrderServiceNotableEvent.E_ITEM_NOT_FOUND, exception.getMessage());
-            }
+            logError(OrderServiceNotableEvent.E_ITEM_NOT_FOUND, itemId);
             throw exception;
         }
 
@@ -144,9 +144,7 @@ public class OrderItemService implements IOrderService {
         orderItem.setItemId(itemId);
 
         orderItemRepository.save(orderItem);
-        if (eventLogger != null) {
-            eventLogger.info(OrderServiceNotableEvent.I_ITEM_ADDED, itemId);
-        }
+        logInfo(OrderServiceNotableEvent.I_ITEM_ADDED, "order=" + orderId + " itemId=" + itemId);
     }
 
     @Override
@@ -161,17 +159,15 @@ public class OrderItemService implements IOrderService {
             case BOOKED:
                 break;
             default:
+                logError(OrderServiceNotableEvent.E_CONFLICT, "'SetTimeSlot' allowed only for 'BOOKED', got order=" + orderId + " status=" + status);
                 throw new BadOperationForCurrentOrderStatus("'SetTimeSlot' allowed only for 'BOOKED' order", orderId, status);
         }
 
         order.setDeliverySlot(time);
         orderRepository.save(order);
 
+        logInfo(OrderServiceNotableEvent.I_ORDER_SET_TIME_SLOT, "order=" + orderId + " slot=" + time);
         var bookingDescription = itemService.describeBooking(order.getLastBookingId());
-
-        if (eventLogger != null) {
-            eventLogger.info(OrderServiceNotableEvent.I_ORDER_SET_TIME, orderId);
-        }
         return new BookingDTO(bookingDescription.getBookingId(), bookingDescription.getFailedItems().keySet());
     }
 
@@ -183,6 +179,7 @@ public class OrderItemService implements IOrderService {
 
         if (!order.getStatus().equals(collectingStatus)) {
             var status = IOrderStatusRepository.StatusNames.valueOf(order.getStatus().getName());
+            logError(OrderServiceNotableEvent.E_CONFLICT, "'FinalizeOrder' allowed only for 'COLLECTING', got order=" + orderId + " status=" + status);
             throw new BadOperationForCurrentOrderStatus("'FinalizeOrder' allowed only for 'COLLECTING' order", orderId, status);
         }
 
@@ -193,24 +190,22 @@ public class OrderItemService implements IOrderService {
         try {
             bookingResult = itemService.book(items);
         } catch (ItemNotFoundException e) {
-            if (eventLogger != null) {
-                eventLogger.error(OrderServiceNotableEvent.E_ITEM_NOT_FOUND, e.getMessage());
-            }
+            logError(OrderServiceNotableEvent.E_ITEM_NOT_FOUND, e.getMessage());
             throw e;
         }
         order.setStatus(statusBooked);
         order.setLastBookingId(bookingResult.getBookingId());
         orderRepository.save(order);
 
-        if (eventLogger != null) {
-            eventLogger.info(OrderServiceNotableEvent.I_ORDER_BOOKED, orderId);
-        }
+        logInfo(OrderServiceNotableEvent.I_ORDER_FINALIZED, orderId);
 
         try {
             /* TODO retrieve adekvatny amount from item service while booking */
             var amount = 42;
             paymentService.submitPayment(userId, orderId, amount, BOOKING_TIMEOUT_MILLIS);
+            logInfo(OrderServiceNotableEvent.I_ORDER_SUBMIT_PAYMENT, orderId);
         } catch (PaymentAlreadyExistsException e) {
+            logError(OrderServiceNotableEvent.E_ORDER_SUBMIT_PAYMENT, orderId);
             throw new RuntimeException("Should not be reached", e);
         }
         return new BookingDTO(bookingResult.getBookingId(), bookingResult.getFailedItems().keySet());
@@ -233,6 +228,7 @@ public class OrderItemService implements IOrderService {
                 order.setStatus(statusPaid);
                 orderRepository.save(order);
                 itemService.completeBooking(order.getLastBookingId());
+                logInfo(OrderServiceNotableEvent.I_ORDER_SUCCESSFUL_PAYMENT, order.getId());
                 if (order.getDeliverySlot() == null) {
                     triggerRefund(order);
                 } else {
@@ -257,6 +253,7 @@ public class OrderItemService implements IOrderService {
                 break;
             case WITHDRAW:
                 /* Ignore and let handlePaymentCancellation() do everything */
+                logInfo(OrderServiceNotableEvent.I_ORDER_FAILED_PAYMENT, order.getId());
                 break;
         }
     }
@@ -270,6 +267,7 @@ public class OrderItemService implements IOrderService {
         OrderStatus statusCollecting = statusRepository.findOrderStatusByName(IOrderStatusRepository.StatusNames.COLLECTING.name());
         order.setStatus(statusCollecting);
         orderRepository.save(order);
+        logInfo(OrderServiceNotableEvent.I_ORDER_CANCELLED_PAYMENT, order.getId());
     }
 
     @Subscribe
@@ -281,9 +279,7 @@ public class OrderItemService implements IOrderService {
         order.setStatus(statusComplete);
         order.setDeliveryDuration(event.getDeliveryDuration());
         orderRepository.save(order);
-        if (eventLogger != null) {
-            eventLogger.info(OrderServiceNotableEvent.I_ORDER_END_DELIVERY, event.getOrderId());
-        }
+        logInfo(OrderServiceNotableEvent.I_ORDER_SUCCESSFUL_DELIVERY, event.getOrderId());
     }
 
     @Subscribe
@@ -291,15 +287,14 @@ public class OrderItemService implements IOrderService {
     public void handleDeliveryFault(DeliveryStatusFailedEvent event) {
         /* PAID -> REFUND */
         triggerRefund(deliveryEventCommonPreHandler(event));
-        if (eventLogger != null) {
-            eventLogger.info(OrderServiceNotableEvent.I_ORDER_FAILED_DELIVERY, event.getOrderId());
-        }
+        logInfo(OrderServiceNotableEvent.I_ORDER_FAILED_DELIVERY, event.getOrderId());
     }
 
     private void triggerRefund(OrderTable order) {
         /* TODO retrieve adekvatny amount from item service while booking */
         var amount = 42;
         eventBus.post(new RefundRequestEvent(order.getId(), order.getUserId(), amount));
+        logInfo(OrderServiceNotableEvent.I_REFUND_STARTED, order.getId());
 
         /* TODO remove onRefundComplete, it should be called on transaction finish! */
         onRefundComplete(order);
@@ -311,6 +306,7 @@ public class OrderItemService implements IOrderService {
         OrderStatus statusRefund = statusRepository.findOrderStatusByName(IOrderStatusRepository.StatusNames.REFUND.name());
         order.setStatus(statusRefund);
         orderRepository.save(order);
+        logInfo(OrderServiceNotableEvent.I_REFUND_DONE, order.getId());
     }
 
     private OrderTable deliveryEventCommonPreHandler(DeliveryStatusEvent event) {
@@ -320,6 +316,7 @@ public class OrderItemService implements IOrderService {
             case SHIPPING:
                 break;
             default:
+                logError(OrderServiceNotableEvent.E_ILLEGAL_STATE, "Delivery status handler for order in state" + order.getStatus().getName());
                 throw new IllegalStateException();
         }
         return order;
@@ -328,6 +325,7 @@ public class OrderItemService implements IOrderService {
     private OrderTable paymentEventCommonPreHandler(PaymentStatusEvent event) {
         OrderTable order = getOrderOrThrow(event.getOrderId(), event.getUserId());
         if (IOrderStatusRepository.StatusNames.valueOf(order.getStatus().getName()) != IOrderStatusRepository.StatusNames.BOOKED) {
+            logError(OrderServiceNotableEvent.E_ILLEGAL_STATE, "Payment status handler for order in state" + order.getStatus().getName());
             throw new IllegalStateException();
         }
         return order;
@@ -336,16 +334,27 @@ public class OrderItemService implements IOrderService {
     private OrderTable getOrderOrThrow(UUID orderId, UUID userId) {
         Optional<OrderTable> orderOptional = orderRepository.findById(orderId);
         if (orderOptional.isEmpty()) {
-            if (eventLogger != null) {
-                eventLogger.error(OrderServiceNotableEvent.E_NO_SUCH_ORDER, orderId);
-            }
+            logError(OrderServiceNotableEvent.E_NO_SUCH_ORDER, orderId);
             throw new OrderNotFoundException("No order with id=" + orderId);
         }
 
         /* Basic user can access only theirs orders, admin can access any order */
         if (!orderOptional.get().getUserId().equals(userId) && !userService.getUserByID(userId).isAdmin()) {
+            logError(OrderServiceNotableEvent.E_NOT_ALLOWED, orderId);
             throw new OrderNotFoundException("No order with id=" + orderId);
         }
         return orderOptional.get();
+    }
+
+    private void logInfo(@NotNull NotableEvent event, Object... argv) {
+        if (eventLogger != null) {
+            eventLogger.info(event, argv);
+        }
+    }
+
+    private void logError(@NotNull NotableEvent event, Object... argv) {
+        if (eventLogger != null) {
+            eventLogger.error(event, argv);
+        }
     }
 }
