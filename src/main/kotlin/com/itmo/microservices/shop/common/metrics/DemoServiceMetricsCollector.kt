dc5e7042bfaae16ec46218dfbe44b065bce68f27
Kozlov Alexey <61562916+OnlyOneUseAcc@users.noreply.github.com>
2022-03-01 05:30:15 +0300

    [metrics] #102 #107 disable DemoServiceMetricsCollector, remove common tag

--- src/main/kotlin/com/itmo/microservices/shop/common/metrics/DemoServiceMetricsCollector.kt
+++ src/main/kotlin/com/itmo/microservices/shop/common/metrics/DemoServiceMetricsCollector.kt
@@ -1,10 +1,9 @@
 package com.itmo.microservices.shop.common.metrics
 
 import com.itmo.microservices.commonlib.metrics.CommonMetricsCollector
-import io.prometheus.client.Counter
-import io.prometheus.client.Gauge
 import org.springframework.stereotype.Component
 
+/*
 @Component
 class DemoServiceMetricsCollector(serviceName: String): CommonMetricsCollector(serviceName) {
     constructor() : this(SERVICE_NAME)
@@ -12,18 +11,5 @@ class DemoServiceMetricsCollector(serviceName: String): CommonMetricsCollector(s
     companion object {
         const val SERVICE_NAME = "p81"
     }
-
-    fun generateGauge(name: String, help: String): Gauge {
-        return Gauge.build()
-                .name(name)
-                .help(help)
-                .register()
-    }
-
-    fun generateCounter(name: String, help: String): Counter {
-        return Counter.build()
-                .name(name)
-                .help(help)
-                .register()
-    }
 }
+*/
