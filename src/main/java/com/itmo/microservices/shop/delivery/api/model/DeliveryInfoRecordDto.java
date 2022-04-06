662e8b15ebcf67c82d3a8c87887f7ebd6a8deda1
Vladislav Yaroshchuk <Vladislav.Yaroshchuk@jetbrains.com>
2022-04-06 03:21:44 +0300

    [delivery] DeliveryInfoRecordDto: drop id from json

--- src/main/java/com/itmo/microservices/shop/delivery/api/model/DeliveryInfoRecordDto.java
+++ src/main/java/com/itmo/microservices/shop/delivery/api/model/DeliveryInfoRecordDto.java
@@ -1,7 +1,6 @@
 package com.itmo.microservices.shop.delivery.api.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
-import lombok.*;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,9 +8,6 @@ import javax.persistence.Id;
 import java.util.UUID;
 
 @Entity
-@Getter
-@Setter
-@ToString
 public class DeliveryInfoRecordDto {
 
     @Id
@@ -24,13 +20,79 @@ public class DeliveryInfoRecordDto {
     private UUID transactionId;
     private long submissionStartedTime;
 
-
-    /* TODO remove */
-    @JsonIgnore
     private UUID orderId;
+
+    @JsonIgnore
+    public UUID getOrderId() {
+        return orderId;
+    }
+
+    @JsonIgnore
+    public void setOrderId(UUID orderId) {
+        this.orderId = orderId;
+    }
+
 
     public DeliveryInfoRecordDto() {
 
+    }
+
+    @JsonIgnore
+    public UUID getId() {
+        return id;
+    }
+
+    @JsonIgnore
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getOutcome() {
+        return outcome;
+    }
+
+    public void setOutcome(String outcome) {
+        this.outcome = outcome;
+    }
+
+    public long getPreparedTime() {
+        return preparedTime;
+    }
+
+    public void setPreparedTime(long preparedTime) {
+        this.preparedTime = preparedTime;
+    }
+
+    public int getAttempts() {
+        return attempts;
+    }
+
+    public void setAttempts(int attempts) {
+        this.attempts = attempts;
+    }
+
+    public long getSubmittedTime() {
+        return submittedTime;
+    }
+
+    public void setSubmittedTime(long submittedTime) {
+        this.submittedTime = submittedTime;
+    }
+
+    public UUID getTransactionId() {
+        return transactionId;
+    }
+
+    public void setTransactionId(UUID transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    public long getSubmissionStartedTime() {
+        return submissionStartedTime;
+    }
+
+    public void setSubmissionStartedTime(long submissionStartedTime) {
+        this.submissionStartedTime = submissionStartedTime;
     }
 
     public enum Outcome {
